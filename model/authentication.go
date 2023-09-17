137c4e6b80b861564e6d6661bea920ab627c0d87
jordanbreen28 <jordan.breen@perforce.com>
2023-09-17 21:42:13 +0100

    (maint) - Update Auth Input Struct

--- model/authentication.go
+++ model/authentication.go
@@ -10,6 +10,4 @@ type RegistrationInput struct {
 type AuthenticationInput struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
-	Email    string `gorm:"size:100;not null;unique" json:"email"`
-	Age      int    `gorm:"not null" json:"age"`
 }
